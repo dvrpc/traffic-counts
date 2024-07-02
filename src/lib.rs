ad912f92364ba3c51444c81af8db920b50e7eaf1
Kris Warner <kdwarn@protonmail.com>
2024-07-02 16:45:37 +0000

    Correct doc on NonNormalVolCount
    No longer created directly from IndividualVehicle, but instead
    from database.

--- src/lib.rs
+++ src/lib.rs
@@ -184,15 +184,12 @@ impl InputCount {
 /// An individual vehicle that has been counted, with no binning applied to it,
 /// including vehicle classification and speed.
 ///
-/// Four different kinds of counts can be derived from this type of data:
+/// Three kinds of counts are derived from this type of data:
 ///   - volume by class per time period ([`TimeBinnedVehicleClassCount`])
 ///   - volume by speed range per time period ([`TimeBinnedSpeedRangeCount`])
-///   - volume per hour of the day ([`denormalize::NonNormalVolCount`])
 ///   - average speed per hour of the day ([`denormalize::NonNormalAvgSpeedCount`])
 ///
-/// See [`create_speed_and_class_count`],
-/// [`denormalize::Denormalize::denormalize_vol_count`], and
-/// [`denormalize::create_non_normal_speedavg_count`].
+/// See [`create_speed_and_class_count`] and [`denormalize::create_non_normal_speedavg_count`].
 #[derive(Debug, Clone)]
 pub struct IndividualVehicle {
     pub date: Date,
